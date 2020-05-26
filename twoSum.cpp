ce3fc1d3f54946364b8b7bf566d47328ea15ef69
Yunchao Yang <yunch.young@gmail.com>
2020-05-26 14:39:17 -0400

    Update twoSum.cpp

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,4 +1,17 @@
+/*
+First attempt for the twoSum problem in Leetcode.
+Use Brute force iterator method in for loop.
+Y. Yang
+To learn:
+1) basic grammar  
+2) nested loop
+3) vector class template: 
+ (1) member types: 
+ (2) Member functions:
+ 基本函数实现 (1) 构造函数 (2)增加函数 (3) 删除函数 遍历函数
 
+  https://www.runoob.com/w3cnote/cpp-vector-container-analysis.html
+*/
 #include <iostream>
 #include <vector>
 
