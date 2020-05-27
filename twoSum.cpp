cd5a00b5fb81a0a86e925ab2bd70d98211431de5
Yunchao Yang <yunch.young@gmail.com>
2020-05-27 11:12:53 -0400

    Update twoSum.cpp

--- twoSum.cpp
+++ twoSum.cpp
@@ -8,8 +8,22 @@ To learn:
 3) vector class template: 
  (1) member types: 
  (2) Member functions:
- 基本函数实现 (1) 构造函数 (2)增加函数 (3) 删除函数 遍历函数
+ vector是一个十分有用的容器,它能够像容器一样存放各种类型的对象
+ 基本函数实现 (1)头文件#include<vector>.
+(2)创建vector对象，vector<int> vec;
+(3)尾部插入数字：vec.push_back(a);
+(4)使用下标访问元素，cout<<vec[0]<<endl;记住下标是从0开始的。
+(5)使用迭代器访问元素.
+vector<int>::iterator it;
+for(it=vec.begin();it!=vec.end();it++)
+    cout<<*it<<endl;
+(6)插入元素：vec.insert(vec.begin()+i,a);在第i+1个元素前面插入a;
+(7)删除元素：    vec.erase(vec.begin()+2);删除第3个元素
+vec.erase(vec.begin()+i,vec.end()+j);删除区间[i,j-1];区间从0开始
+(8)向量大小:vec.size();(9)清空:vec.clear();
+————————————————
 
+https://blog.csdn.net/duan19920101/java/article/details/50617190
   https://www.runoob.com/w3cnote/cpp-vector-container-analysis.html
 */
 #include <iostream>
