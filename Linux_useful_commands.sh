cdbf8fa47b298b5951adf976cd3edd956c360e1b
Yunchao Yang <yunch.young@gmail.com>
2020-10-06 12:22:13 -0400

    Update Linux_useful_commands.sh

--- Linux_useful_commands.sh
+++ Linux_useful_commands.sh
@@ -4,3 +4,13 @@
 
 - What is fork?
   - fork is an operation whereby a process creates a copy of itself. 
+
+- What is Linux Kernal? 
+  - Linux Kernal is considered as free and open-source software which is capable of managing hardware resources for the users. As it is released under General Public Licence (GPL), it becomes legal for anyone to edit it.
+
+-  What are the basic components of Linux?
+  - Kernel: It is the core component of the Linux, it acts as an interface between software and hardware.  
+  - Shell: It acts as an interface between the user and the Kernel.
+  - GUI:  It stands for Graphic User Interface, which is another way for the user to interact with the system. But it is unlike images, buttons, text boxes for interaction.
+  - System Utilities: These are the software functions that allow users to manage the computer.
+  - Application Programs: Set of functions designed to perform a set of tasks.
