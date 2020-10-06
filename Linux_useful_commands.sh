b2038db7e2de774658f2e7dd35539a1b33d8e42f
Yunchao Yang <yunch.young@gmail.com>
2020-10-06 12:25:50 -0400

    Update Linux_useful_commands.sh

--- Linux_useful_commands.sh
+++ Linux_useful_commands.sh
@@ -1,4 +1,4 @@
- - What is the difference between executing a Bash script vs sourcing it?
+- What is the difference between executing a Bash script vs sourcing it?
    - Sourcing a script will run the commands in the current shell process.
    - Executing a script will run the commands in a new shell process.
 
@@ -14,3 +14,11 @@
    - GUI:  It stands for Graphic User Interface, which is another way for the user to interact with the system. But it is unlike images, buttons, text boxes for interaction.
    - System Utilities: These are the software functions that allow users to manage the computer.
    - Application Programs: Set of functions designed to perform a set of tasks.
+   
+Q #3) Enlist the basic components of LINUX?
+
+Answer: Linux operating system basically consists of 3 components. They are:
+
+Kernel: This is considered as the core part and is responsible for all major activities of the Linux operating system. Linux Kernel is considered as free and open-source software that is capable of managing hardware resources for the users. It consists of various modules and interacts directly with the underlying hardware.
+System Library: Most of the functionalities of the operating system are implemented by System Libraries. These act as a special function using which application programs accesses Kernel’s features.
+System Utility: These programs are responsible for performing specialized, individual-level tasks.
