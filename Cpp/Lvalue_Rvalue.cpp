2a9c1689dd7887309e5f8253780a9a7b53cc4987
Yunchao Yang <yunch.young@gmail.com>
2020-11-27 14:11:33 -0500

    Update Lvalue_Rvalue.cpp

--- Cpp/Lvalue_Rvalue.cpp
+++ Cpp/Lvalue_Rvalue.cpp
@@ -43,8 +43,41 @@ dog d1; //lvalue of user defined types;
  square(40); //error
  
  //How to make square(40) work?
- 
+ // Walkaround
  int square1(const int& x) {return x*x;} // both square1(40) and square1(i) work.
 
- 
- 
+ // lvalue can be used to create a rvalue
+int i = 1;
+int x = i+2;
+
+int x = i; // a lvalue can be implicitly transformed to a rvalue.
+
+//rvalue can be explicitly used to create a lvalue
+int v[3];
+*(v+2) = 4; // *(v+2) is a lvalue
+
+//Misconcept 1: Function or operator always yields rvalue;
+int x = i+3;
+int y = sum(3,4);
+
+// Counterexample 1
+int myglobal;
+int& foo(){
+return myglobal;
+}
+foo() = 50;
+
+// Counterexample 2
+array[3] = 50; //operator [] almost always gnerates a lvalue;
+
+//Misconcept 2: Lvalue are modifiable;
+const int c = 1; // c is a lvalue
+c = 2; // error
+
+//Misconcept 3: Rvalue are not modifiable;
+i+3 = 6;
+sum(3,4) = 7;
+
+// However, It is not true for user defined type(class)
+class dog;
+dog().bark(); // dog() is rvalue, is modifiable.
